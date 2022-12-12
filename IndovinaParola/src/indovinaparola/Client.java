f7dee36584dac8b0869145d71c94127c41dcd519
Luca Beccalli <103634497+lucabecca@users.noreply.github.com>
2022-12-12 09:03:02 +0100

    Implementazione Client

--- IndovinaParola/src/indovinaparola/Client.java
+++ IndovinaParola/src/indovinaparola/Client.java
@@ -18,16 +18,18 @@ import javax.swing.SwingUtilities;
  * @author becca
  */
 public class Client {
-
+    
     private Scanner scan;
     private Socket socket = null;
     private DataInputStream input = null;
     private DataOutputStream output = null;
     private InetAddress ip;
     private int PORT = 1234;
-
+    
     private boolean logout;
-
+    
+    private int primoMess;
+    
     public Client() {
         try {
             // TO DO Lo chiederà all'avvio
@@ -39,11 +41,13 @@ public class Client {
             input = new DataInputStream(socket.getInputStream());
             // Per inviare i dati
             output = new DataOutputStream(socket.getOutputStream());
-
-            scan = new Scanner(System.in);
             
+            scan = new Scanner(System.in);
+
             // Inizializzo logout
             logout = false;
+            
+            primoMess = 0;
         } catch (UnknownHostException ex) {
             // Output eccezione
             System.out.println("Client : " + ex.getMessage());
@@ -51,24 +55,25 @@ public class Client {
             // Output eccezione
             System.out.println("Client : " + ex.getMessage());
         }
-
+        
     }
-
+    
     public static void main(String[] args) {
+        
         Client client = new Client();
-        System.out.println("IMPORTANTE, guardare riga 115-116 del file Client che spiega la sintassi da usare per mandare i messaggi al server");
         // Avvio il Thread per la recezione dei messaggi
         client.readMessageThread();
         // Avvio il Thread per l'invio dei messaggi
         client.writeMessageThread();
+
         // Grafica
         openJFrames();
     }
-
+    
     private void readMessageThread() {
         // Creo il Thread
         Thread readMessage = new Thread(new Runnable() {
-
+            
             @Override
             public void run() {
                 // Ciclo infinito
@@ -76,39 +81,53 @@ public class Client {
                     try {
                         // Leggo il messaggio
                         String msg = input.readUTF();
-                        System.out.println(msg);
+
+                        // Se il server disconnette per vittoria
+                        if (msg.trim().equals("logout")) {
+                            logout = true;
+                            System.out.println("Hai vinto!\nPremi invio per uscire");
+                        } else {
+                            // Azione che fa solo al primo messaggio
+                            if (primoMess == 0) {
+                                System.out.println(msg);
+                                primoMess++;
+                            } else {
+                                System.out.println("Stato parola: " + msg);
+                            }
+                        }
+                        
                     } catch (IOException ex) {
                         // Output eccezione
                         System.out.println("readMessageThread : " + ex.getMessage());
                     }
-
+                    
                 }
             }
         });
         // Avvio il Thread
         readMessage.start();
     }
-
+    
     private void writeMessageThread() {
         Thread sendMessage = new Thread(new Runnable() {
-
+            
             @Override
             public void run() {
                 System.out.println("Inserire \"logout\" per disconnettersi.");
+                String msg;
                 // Ciclo infinito
                 while (!logout) {
+
                     // Leggo messaggio inserito dall'utente
-                    String msg = scan.nextLine();
+                    msg = scan.nextLine();
 
                     // Se l'utente vuole disconnettersi
-                    if (msg.equals("logout")) {
+                    if (msg.trim().equals("logout")) {
                         logout = true;
                     }
-
+                    
                     try {
-                        // Invia un messaggio e aggiunte il nome del client e # se non il server non funziona
-                        
-                        // Invia messaggio (da aggiunte manualmente il nome del client e #)
+                        // Invia messaggio
                         output.writeUTF(msg);
                     } catch (IOException ex) {
                         // Output eccezione
@@ -120,15 +139,14 @@ public class Client {
         // Avvio il Thread
         sendMessage.start();
     }
-
+    
     private static void openJFrames() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 // Inizializza il frame 1
                 JFrameOne frame1 = new JFrameOne();
-                // Inizializza il frame 2
-                JFrameTwo frame2 = new JFrameTwo();
+                frame1.setVisible(true);
             }
         });
     }
--- IndovinaParola/src/indovinaparola/JFrameOne.java
+++ IndovinaParola/src/indovinaparola/JFrameOne.java
@@ -4,14 +4,29 @@
  */
 package indovinaparola;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.naming.InitialContext;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author becca
  */
 public class JFrameOne extends JFrame {
+
+    // JButton btnConnetti
+    private JButton btnConnetti;
 
     public JFrameOne() {
         initialize();
@@ -20,9 +35,60 @@ public class JFrameOne extends JFrame {
     private void initialize() {
         setTitle("Frame one");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500,400);
+        setSize(500, 100);
         setLocationRelativeTo(null);
-        setVisible(true);
+
+        // JPanel
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 5));
+
+        // JLabel
+        JLabel lbl = new JLabel("Inserisci l'IP del server:");
+        lbl.setFont(new Font("Arial", Font.PLAIN, 18));
+        panel.add(lbl);
+
+        // JTextField
+        JTextField fldIp = creaTextFieldIP();
+        panel.add(fldIp);
+
+        // JButton btnConnetti
+        btnConnetti = creaButtonConnetti();
+        panel.add(btnConnetti);
+
+        // add frame
+        add(panel, BorderLayout.CENTER);
     }
 
+    private JButton creaButtonConnetti() {
+        JButton btnConnetti = new JButton("Connetti");
+
+        btnConnetti.setFocusable(false);
+        btnConnetti.setToolTipText("Connettiti al server");
+        btnConnetti.setFont(new Font("Arial", Font.PLAIN, 18));
+
+        btnConnetti.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+        });
+
+        return btnConnetti;
+    }
+
+    private JTextField creaTextFieldIP() {
+        JTextField fldIp = new JTextField(10);
+
+        fldIp.setToolTipText("IP del server");
+        fldIp.setFont(new Font("Arial", Font.PLAIN, 18));
+
+        fldIp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                btnConnetti.doClick();
+            }
+        });
+
+        return fldIp;
+    }
 }
